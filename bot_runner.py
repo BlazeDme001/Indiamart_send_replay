403a742d5d4a973efc4f0f06b897478a117ddc61
Shreenath Group <132436285+BlazeDme001@users.noreply.github.com>
2024-03-21 16:15:08 +0530

    Update bot_runner.py
    Added time loop for 23:59 hrs

--- bot_runner.py
+++ bot_runner.py
@@ -3,16 +3,21 @@ from main import run_bot
 import time
 
 if __name__ == '__main__':
-    keywords_list = ['fire resistant doors', 'lead lined doors']
-    quantities_list = [10, 2]
+    while True:
+        print('BOT Started')
+        keywords_list = ['fire resistant doors', 'lead lined doors']
+        quantities_list = [10, 2]
+    
+        processes = []
+        for keywords, quantities in zip(keywords_list, quantities_list):
+            process = Process(target=run_bot, args=(keywords, quantities))
+            processes.append(process)
+            process.start()
+            time.sleep(30)
+    
+        for process in processes:
+            process.join()
 
-    processes = []
-    for keywords, quantities in zip(keywords_list, quantities_list):
-        process = Process(target=run_bot, args=(keywords, quantities))
-        processes.append(process)
-        process.start()
-        time.sleep(30)
-
-    for process in processes:
-        process.join()
-
+        print('BOT Ended')
+        print('BOT will start after 23:59 hours')
+        time.sleep(86400)
