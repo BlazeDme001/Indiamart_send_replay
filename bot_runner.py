30c96f40b16d68a595f33e4e900453c0d496a05c
Shreenath Group <132436285+BlazeDme001@users.noreply.github.com>
2024-03-30 16:31:09 +0530

    Add files via upload

--- bot_runner.py
+++ bot_runner.py
@@ -4,10 +4,10 @@ import time
 
 if __name__ == '__main__':
     keywords_list = ['fire resistant doors', 'lead lined doors']
-    quantities_list = [30, 5]
+    quantities_list = [25, 25]
 
     while True:
-        print('BOT Start')
+        print('BOT started')
         processes = []
         for keywords, quantities in zip(keywords_list, quantities_list):
             process = Process(target=run_bot, args=(keywords, quantities))
@@ -17,5 +17,5 @@ if __name__ == '__main__':
 
         for process in processes:
             process.join()
-        print('BOt Stop. It will start after 2 min')
-        time.sleep(15*120)
+        print('BOT will start after 30 minutes')
+        time.sleep(30*60)
